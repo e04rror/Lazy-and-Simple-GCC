28a58fb55f8dceffad03d7c200939deaa8ece6d9
e04rror <vaniaua9@gmail.com>
2024-06-07 00:08:53 +0300

    Initialization the basic function in .hpp file

--- functions.hpp
+++ functions.hpp
@@ -1,7 +1,17 @@
 #ifndef FUNCTION_HPP
 #define FUNCTION_HPP
 
-const unsigned int MAX_AMOUNT_OF_ARGUMENTS = 2;
+#include <iostream> // For std
+
+const unsigned int MAX_AMOUNT_OF_ARGUMENTS = 2; // for the first time, the program only takes two arguments
+
+
+// Execution filename will be inheritance from the cpp/c file 
+// For example, if you have main.cpp execution file will be main, if test.c - test and so on
+std::string makeExecutionFilename(char* argv);
+
+// This is the main process it will create line like this "gcc test.c -o test" from the one argument(the second one from argv array)
+std::string makeCommandFunction(char* argv);
 
 #endif 
 
