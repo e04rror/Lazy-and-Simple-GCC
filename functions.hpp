089328038d219ce325429a8c62da53988583fd28
e04rror <vaniaua9@gmail.com>
2024-06-06 12:43:25 +0300

    Create header file

---
+++ functions.hpp
@@ -0,0 +1,8 @@
+#ifndef FUNCTION_HPP
+#define FUNCTION_HPP
+
+const unsigned int MAX_AMOUNT_OF_ARGUMENTS = 2;
+
+#endif 
+
+
--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+// create function that execute filename that we get
+#include <iostream>
+#include <cstdlib>
+#include "functions.hpp"
 
+
+
+
+std::string makeCommandFunction(char* argv) {
+  // Create function that makes g++/gcc command  
+}
+
+int main(int argc, char* argv[]) {
+  // At first, I want my program take only two arguments(I mean kind like test)
+  if(argc == MAX_AMOUNT_OF_ARGUMENTS) {
+    std::string commandName = "g++ -std=23" + std::string(argv[1]);
+    system(commandName.c_str());
+  }
+  return 0;
+}
