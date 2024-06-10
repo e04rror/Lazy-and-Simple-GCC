7435f347b026759945b0e2dffd8d687517aa0b23
e04rror <vaniaua9@gmail.com>
2024-06-10 11:30:51 +0300

    Made a function that creates a command from substrings

--- functions.cpp
+++ functions.cpp
@@ -1,43 +1,64 @@
-// First create with g++
-
+#include <iostream>
 #include "functions.hpp"
 #include <cstdlib>
 #include <string>
-#include <utility>
-#include <algorithm>
 
-std::string checkFileExtension(const std::string& argv,  const std::pair<std::string, std::string> extension){
+std::string
+checkFileExtension(const std::string &argv,
+                   const std::pair<std::string, std::string> extension) {
   std::string keyExtension = extension.first;
-  
-  bool isSizeNamefileBiggerThanKey = argv.length() >= keyExtension.length();
-  bool findExtension = (argv.rfind(keyExtension) == argv.length() - keyExtension.length());
 
-  if(isSizeNamefileBiggerThanKey && findExtension){
+  bool isSizeNamefileBiggerThanKey = argv.length() >= keyExtension.length();
+  bool findExtension =
+      (argv.rfind(keyExtension) == argv.length() - keyExtension.length());
+
+  if (isSizeNamefileBiggerThanKey && findExtension) {
     return extension.second;
   }
 
   return {};
 }
 
-std::string createExecutionFile(const std::string& argv, const std::string& removeElement) {
-   size_t removingPosition = argv.rfind(removeElement);
-  
-  //Let me explain, I decided to create a new variable that simply copies argv
-  //You might ask why I don't just pass the value directly (remove the &) 
-  //Well, in that case, our function would return its argument,that it gives , which is not good practice.
+std::string createExecutionFile(const std::string &argv,
+                                const std::string &removeElement) {
+  size_t removingPosition = argv.rfind(removeElement);
+
+  // Let me explain, I decided to create a new variable that simply copies argv
+  // You might ask why I don't just pass the value directly (remove the &)
+  // Well, in that case, our function would return its argument,that it gives ,
+  // which is not good practice.
   std::string nameExecutionFile = argv;
-   if(removingPosition!= std::string::npos){
-    nameExecutionFile.erase(removingPosition, removeElement.length());  
+  if (removingPosition != std::string::npos) {
+    nameExecutionFile.erase(removingPosition, removeElement.length());
   }
   return nameExecutionFile;
 }
 
-std::string makeCommandFunction(int argc, char* argv){
+std::pair<std::string, std::string>
+determineAndCreateExeFileAndExtension(const std::string &argv) {
+  // Need to create exception handle 
+  std::string whatTypeIs = checkFileExtension(argv, C_EXTENSION);
+  std::string executionNameFile;
+  if (whatTypeIs.empty()) {
+    whatTypeIs = checkFileExtension(argv, CPP_EXTENSION);
+    executionNameFile = createExecutionFile(argv, CPP_EXTENSION.first);
+  } else {
+    std::string executionNameFile =
+        createExecutionFile(argv, C_EXTENSION.first);
+  }
+  return {whatTypeIs, executionNameFile};
+}
+
+std::string makeCommandFunction(int argc,const std::string& argv) {
   // At first, I want my program take only two arguments(I mean kind like test)
+  if(argc != MAX_AMOUNT_OF_ARGUMENTS) {
+    std::cerr<<"Amount of arguments must be 2!"<<std::endl;
+    return "";  
+  }
 
-  // Then it must be check what is the file extension(c or c++)
+  auto extensionAndExecution = determineAndCreateExeFileAndExtension(argv);
 
-  //
+  std::string result = extensionAndExecution.first +" " + argv + " -o " + extensionAndExecution.second;
   
-
+  return result;
 }
--- main.cpp
+++ main.cpp
@@ -6,15 +6,9 @@
 
 int main(int argc, char* argv[]) {
   const std::string secondArg = argv[1];
-  // First test
-  std::string whatTypeIs = checkFileExtension(secondArg, C_EXTENSION);
-  std::string executionNameFile = createExecutionFile(secondArg, C_EXTENSION.first);
-  if(whatTypeIs.empty()){
-    whatTypeIs = checkFileExtension(secondArg, CPP_EXTENSION);
-    executionNameFile = createExecutionFile(secondArg, CPP_EXTENSION.first);
-  }
 
-  std::cout<<"Extension of the file: "<<whatTypeIs<<std::endl;
-  std::cout<<"Executional file: "<<executionNameFile<<std::endl;
-  return 0;
+  std::string command = makeCommandFunction(argc, secondArg);
+  std::cout<<"Command: "<<command<<std::endl;
+
+ return 0;
 }
